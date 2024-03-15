a0f4efcaa3bcd98a0504ed88b29271da084d42e6
K-M-Shehan <miweshshehan@gmail.com>
2024-03-15 12:45:16 +0530

    update code.c

--- main/part-1a/code.c
+++ main/part-1a/code.c
@@ -1,8 +1,44 @@
-/*
-*Pseudocode
-*menu to select battleship
-*
-*
-*
-*
-*/
+#include <SDL2/SDL.h>
+#define WINDOW_SIZE 400
+
+int main (void) 
+{
+
+  // Initialize SDL
+  if (SDL_Init(SDL_INIT_VIDEO) < 0)
+  {
+    printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+    return 1;
+  }
+
+  // Creating a window
+  SDL_WINDOW* window = SDL_CreateWindow("Naval Warfare Simulator", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_SIZE, WINDOW_SIZE, SDL_WINDOW_SHOWN);
+  
+  // Show error if window is not created
+  if (window == NULL)
+  {
+    printf("Window could not be created! SDL_Error: %s\n", SDL_Erro());
+    return 1;
+  }
+
+  // Quiting
+  int quit = 0;
+  SDL_Event e;
+
+  while (!quit)
+  {
+    while (SDL_PollEvent(&e) != 0)
+    {
+      if (e.type == SDL_QUIT)
+      {
+        quit = 1;
+      }
+    }
+  }
+
+  // Cleanup
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+
+  return 0;
+}
