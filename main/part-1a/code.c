6819d857608ccfeedd825450a29b42dacaf33473
K-M-Shehan <miweshshehan@gmail.com>
2024-03-15 12:54:08 +0530

    update code.ino & create nbs

--- main/part-1a/code.c
+++ main/part-1a/code.c
@@ -12,12 +12,12 @@ int main (void)
   }
 
   // Creating a window
-  SDL_WINDOW* window = SDL_CreateWindow("Naval Warfare Simulator", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_SIZE, WINDOW_SIZE, SDL_WINDOW_SHOWN);
+  SDL_Window* window = SDL_CreateWindow("Naval Warfare Simulator", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_SIZE, WINDOW_SIZE, SDL_WINDOW_SHOWN);
   
   // Show error if window is not created
   if (window == NULL)
   {
-    printf("Window could not be created! SDL_Error: %s\n", SDL_Erro());
+    printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
     return 1;
   }
 
