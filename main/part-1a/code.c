441d161b0d280a8ef5737c76fdb36b3fb8e5a634
K-M-Shehan <miweshshehan@gmail.com>
2024-03-17 09:20:30 +0530

    functions added/optimize main function

--- main/part-1a/code.c
+++ main/part-1a/code.c
@@ -1,45 +1,76 @@
+#include <stdio.h>
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 #define WINDOW_SIZE 700
 
-SDL_Window* window = NULL;
-SDL_Renderer* renderer = NULL;
+typedef struct
+{
+  int x, y;         // position
+  short state;      // active or destroyed
+  char* timeActive; // time spent in simulator
+  char* name;       // name of escort ship       
+  int id;           // unique id
+} EscortShip;
 
-void initializeSDL (void) 
+void initializeSDL (SDL_Window* window, SDL_Renderer* renderer) 
 {
   // SDL Initialization
   SDL_Init(SDL_INIT_VIDEO);
   // Creating a window
-  window = SDL_CreateWindow("Naval Warfare Simulator", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_SIZE, WINDOW_SIZE, SDL_WINDOW_SHOWN);
+  window = SDL_CreateWindow("Naval Warfare Simulator", 
+                            SDL_WINDOWPOS_UNDEFINED, 
+                            SDL_WINDOWPOS_UNDEFINED, 
+                            WINDOW_SIZE, 
+                            WINDOW_SIZE, 
+                            SDL_WINDOW_SHOWN
+                            );
   // Creating a renderer for the window
-  renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED); 
+  renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC); 
 }
 
-void cleanup (void)
+int processEvents (SDL_Window* window)
+{
+  SDL_Event event;
+  int done = 0;
+
+     while (SDL_PollEvent(&event))
+      {
+        switch (event.type)
+        {
+          case SDL_QUIT:
+            done = 1;
+          break;
+          
+        }
+      }
+
+  return done;
+}
+
+void cleanup (SDL_Window* window, SDL_Renderer* renderer)
 {
    // Cleanup
   SDL_DestroyWindow(window);
+  SDL_DestroyRenderer(renderer);
   SDL_Quit();
 }
 
 int main (void) 
 {
-  initializeSDL();
+  SDL_Window* window = NULL;
+  SDL_Renderer* renderer = NULL;
 
-   // Quiting
-  int quit = 0;
-  SDL_Event e;
+  initializeSDL(window, renderer);
 
-  while (!quit)
-  {
-    while (SDL_PollEvent(&e) != 0)
-    {
-      if (e.type == SDL_QUIT)
-      {
-        quit = 1;
-      }
-    }
+  // Running events
+  int done = 0;
+  while (!done)
+  {  
+    // Check for events
+    done = processEvents(window);
   }
 
-  cleanup();
+  // Free memory
+  cleanup(window, renderer);
   return 0;
 }
